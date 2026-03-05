e12f7588d57644b4baf188fe7b64ea489181d7d3
Gérald Barré <gerald.barre@workleap.com>
2026-03-05 12:44:09 -0500

    Refactor spectral rules to distinguish between backend and frontend type requirements; add new test specifications for valid compositions. (#44)
    Co-authored-by: Gérald Barré <gerald.barre@gsoft.com>

--- test.ps1
+++ test.ps1
@@ -8,9 +8,14 @@ spectral --version
 $ruleset = Join-Path $PSScriptRoot ".workleap.rules.yaml"
 
 $testSpecs = @(
-    @{ rule = "items-must-have-a-type"; expectError = $false; filename = "items-must-have-a-type-valid.yaml" },
-    @{ rule = "items-must-have-a-type"; expectError = $false; filename = "items-must-have-a-type-valid-ref.yaml" },
-    @{ rule = "items-must-have-a-type"; expectError = $true; filename = "items-must-have-a-type-invalid.yaml" },
+    @{ rule = "items-must-have-a-type-backend"; expectError = $false; filename = "items-must-have-a-type-valid.yaml" },
+    @{ rule = "items-must-have-a-type-backend"; expectError = $false; filename = "items-must-have-a-type-valid-ref.yaml" },
+    @{ rule = "items-must-have-a-type-backend"; expectError = $true; filename = "items-must-have-a-type-valid-composition.yaml" },
+    @{ rule = "items-must-have-a-type-backend"; expectError = $true; filename = "items-must-have-a-type-invalid.yaml" },
+    @{ rule = "items-must-have-a-type-frontend"; expectError = $false; filename = "items-must-have-a-type-valid.yaml" },
+    @{ rule = "items-must-have-a-type-frontend"; expectError = $false; filename = "items-must-have-a-type-valid-ref.yaml" },
+    @{ rule = "items-must-have-a-type-frontend"; expectError = $false; filename = "items-must-have-a-type-valid-composition.yaml" },
+    @{ rule = "items-must-have-a-type-frontend"; expectError = $true; filename = "items-must-have-a-type-invalid.yaml" },
     @{ rule = "must-accept-content-types"; expectError = $false; filename = "must-accept-content-types-valid.yaml" },
     @{ rule = "must-accept-content-types"; expectError = $true; filename = "must-accept-content-types-invalid.yaml" },
     @{ rule = "must-not-use-base-server-url"; expectError = $false; filename = "must-not-use-base-server-url-valid.yaml" },
@@ -21,17 +26,32 @@ $testSpecs = @(
     @{ rule = "must-support-client-credentials-oauth2"; expectError = $true; filename = "must-support-client-credentials-oauth2-invalid.yaml" },
     @{ rule = "must-use-get-post-methods"; expectError = $false; filename = "must-use-get-post-methods-valid.yaml" },
     @{ rule = "must-use-get-post-methods"; expectError = $true; filename = "must-use-get-post-methods-invalid.yaml" },
-    @{ rule = "path-schema-properties-must-have-a-type"; expectError = $false; filename = "path-schema-properties-must-have-a-type-valid.yaml" },
-    @{ rule = "path-schema-properties-must-have-a-type"; expectError = $false; filename = "path-schema-properties-must-have-a-type-valid-ref.yaml" },
-    @{ rule = "path-schema-properties-must-have-a-type"; expectError = $true; filename = "path-schema-properties-must-have-a-type-invalid.yaml" },
-    @{ rule = "schemas-properties-must-have-a-type"; expectError = $false; filename = "schemas-properties-must-have-a-type-valid.yaml" },
-    @{ rule = "schemas-properties-must-have-a-type"; expectError = $false; filename = "schemas-properties-must-have-a-type-valid-ref.yaml" },
-    @{ rule = "schemas-properties-must-have-a-type"; expectError = $true; filename = "schemas-properties-must-have-a-type-invalid.yaml" },
+    @{ rule = "path-schema-properties-must-have-a-type-backend"; expectError = $false; filename = "path-schema-properties-must-have-a-type-valid.yaml" },
+    @{ rule = "path-schema-properties-must-have-a-type-backend"; expectError = $false; filename = "path-schema-properties-must-have-a-type-valid-ref.yaml" },
+    @{ rule = "path-schema-properties-must-have-a-type-backend"; expectError = $true; filename = "path-schema-properties-must-have-a-type-valid-composition.yaml" },
+    @{ rule = "path-schema-properties-must-have-a-type-backend"; expectError = $true; filename = "path-schema-properties-must-have-a-type-invalid.yaml" },
+    @{ rule = "path-schema-properties-must-have-a-type-frontend"; expectError = $false; filename = "path-schema-properties-must-have-a-type-valid.yaml" },
+    @{ rule = "path-schema-properties-must-have-a-type-frontend"; expectError = $false; filename = "path-schema-properties-must-have-a-type-valid-ref.yaml" },
+    @{ rule = "path-schema-properties-must-have-a-type-frontend"; expectError = $false; filename = "path-schema-properties-must-have-a-type-valid-composition.yaml" },
+    @{ rule = "path-schema-properties-must-have-a-type-frontend"; expectError = $true; filename = "path-schema-properties-must-have-a-type-invalid.yaml" },
+    @{ rule = "schemas-properties-must-have-a-type-backend"; expectError = $false; filename = "schemas-properties-must-have-a-type-valid.yaml" },
+    @{ rule = "schemas-properties-must-have-a-type-backend"; expectError = $false; filename = "schemas-properties-must-have-a-type-valid-ref.yaml" },
+    @{ rule = "schemas-properties-must-have-a-type-backend"; expectError = $true; filename = "schemas-properties-must-have-a-type-valid-composition.yaml" },
+    @{ rule = "schemas-properties-must-have-a-type-backend"; expectError = $true; filename = "schemas-properties-must-have-a-type-invalid.yaml" },
+    @{ rule = "schemas-properties-must-have-a-type-frontend"; expectError = $false; filename = "schemas-properties-must-have-a-type-valid.yaml" },
+    @{ rule = "schemas-properties-must-have-a-type-frontend"; expectError = $false; filename = "schemas-properties-must-have-a-type-valid-ref.yaml" },
+    @{ rule = "schemas-properties-must-have-a-type-frontend"; expectError = $false; filename = "schemas-properties-must-have-a-type-valid-composition.yaml" },
+    @{ rule = "schemas-properties-must-have-a-type-frontend"; expectError = $true; filename = "schemas-properties-must-have-a-type-invalid.yaml" },
     @{ rule = "schema-ids-must-have-alphanumeric-characters-only"; expectError = $false; filename = "schema-ids-must-have-alphanumeric-characters-only-valid.yaml" },
     @{ rule = "schema-ids-must-have-alphanumeric-characters-only"; expectError = $true; filename = "schema-ids-must-have-alphanumeric-characters-only-invalid.yaml" },
-    @{ rule = "schema-object-must-have-a-type"; expectError = $false; filename = "schema-object-must-have-a-type-valid.yaml" },
-    @{ rule = "schema-object-must-have-a-type"; expectError = $false; filename = "schema-object-must-have-a-type-valid-ref.yaml" },
-    @{ rule = "schema-object-must-have-a-type"; expectError = $true; filename = "schema-object-must-have-a-type-invalid.yaml" },
+    @{ rule = "schema-object-must-have-a-type-backend"; expectError = $false; filename = "schema-object-must-have-a-type-valid.yaml" },
+    @{ rule = "schema-object-must-have-a-type-backend"; expectError = $false; filename = "schema-object-must-have-a-type-valid-ref.yaml" },
+    @{ rule = "schema-object-must-have-a-type-backend"; expectError = $true; filename = "schema-object-must-have-a-type-valid-composition.yaml" },
+    @{ rule = "schema-object-must-have-a-type-backend"; expectError = $true; filename = "schema-object-must-have-a-type-invalid.yaml" },
+    @{ rule = "schema-object-must-have-a-type-frontend"; expectError = $false; filename = "schema-object-must-have-a-type-valid.yaml" },
+    @{ rule = "schema-object-must-have-a-type-frontend"; expectError = $false; filename = "schema-object-must-have-a-type-valid-ref.yaml" },
+    @{ rule = "schema-object-must-have-a-type-frontend"; expectError = $false; filename = "schema-object-must-have-a-type-valid-composition.yaml" },
+    @{ rule = "schema-object-must-have-a-type-frontend"; expectError = $true; filename = "schema-object-must-have-a-type-invalid.yaml" },
     @{ rule = "schema-name-length-must-be-short"; expectError = $false; filename = "schema-name-length-must-be-short-valid.yaml" },
     @{ rule = "schema-name-length-must-be-short"; expectError = $true; filename = "schema-name-length-must-be-short-invalid.yaml" }
 )
@@ -39,8 +59,9 @@ $testSpecs = @(
 function RunSpectralTests($ruleset, $tests, $testSpecsPath)
 {
     $fileCount = Get-ChildItem (Join-Path $PSScriptRoot $testSpecsPath) | Measure-Object | Select-Object -ExpandProperty Count
-    if ($tests.Count -ne $fileCount) {
-        throw "Number of tests does not match number of specs. Add the missing specs to the `$tests variable. Files: $fileCount, Tests: $($tests.Count)"
+    $uniqueTestFiles = $tests | ForEach-Object { $_.filename } | Sort-Object -Unique | Measure-Object | Select-Object -ExpandProperty Count
+    if ($uniqueTestFiles -ne $fileCount) {
+        throw "Number of unique test spec files does not match number of specs. Add the missing specs to the `$tests variable. Files: $fileCount, Unique test files: $uniqueTestFiles"
     }
 
     # We expect spectral to fail, so we need to capture the error
